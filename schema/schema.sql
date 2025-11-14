c57abeaaccda567cd5f626ed675cc19ab00c24dc
Rachel Teano <rpteano@gmail.com>
2025-11-14 10:56:13 -0500

    added tables to schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,5 +3,28 @@ USE mysql_project_db;
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(200) NOT NULL
+  password VARCHAR(200) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE books (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  api_source VARCHAR(50) NOT NULL,
+  api_id VARCHAR(200) NOT NULL,
+  title VARCHAR(300) NOT NULL,
+  author VARCHAR(300) NOT NULL,
+  thumbnail_url VARCHAR(500) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY unique_api_book (api_source, api_id)
+);
+
+CREATE TABLE user_books (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  book_id INT NOT NULL,
+  status ENUM('to_read','reading','finished'),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY unique_user_book (user_id, book_id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
+)
