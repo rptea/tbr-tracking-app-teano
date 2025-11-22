c4693e89159d8b3c83d527ce8783ea6d33b78e5b
Rachel Teano <rpteano@gmail.com>
2025-11-22 02:14:41 -0500

    modified bookController.js, added services/bookApiService.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,3 +1,6 @@
+// Imports
+const bookApiService = require('../services/bookApiService')
+
 // Show search form
 function showSearchForm(req, res) {
     res.render('search');
@@ -8,9 +11,10 @@ async function handleSearch(req, res) {
     try {
         const query = req.body.query;
 
-        if (!query || query.trim()==='') {
+        if (!query || query.trim() === '') {
             return res.render('search', { error: 'Please enter a search term'});
         }
+
         const results = await bookApiService.searchBooks(query);
         return res.render('results', { results, query });
     } catch (err) {
@@ -21,5 +25,41 @@ async function handleSearch(req, res) {
 
 // Save a selected book to books and user_books
 async function saveBook(req, res) {
-     
+    try {
+        const userId = res.session.userId
+
+        if (!userId) {
+            return res.redirect('/login')
+        }
+
+        const {
+            api_source,
+            api_id,
+            title,
+            author,
+            thumbnail_url,
+        } = req.body
+
+        if (!api_source || !api_id || !title) {
+            return res.status(400).send('Missing book data')
+        }
+
+        let existingBook = await bookApiService.findByApiId(api_source, api_id)
+        let bookId
+
+        if (!existingUserBook) {
+            await UserBook.create(userId, bookId)
+        }
+
+        return res.redirect('/books/saved')
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error saving book')
+    }
+}
+
+module.exports = {
+    showSearchForm,
+    handleSearch,
+    saveBook
 }
---
+++ services/bookApiService.js
@@ -0,0 +1,1 @@
+async function searchBooks (query)
