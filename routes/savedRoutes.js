280d9c578f2eac6d3f26b31a75ad3f49947687a3
Rachel Teano <rpteano@gmail.com>
2025-11-22 10:48:19 -0500

    modified savedRoutes.js

---
+++ routes/savedRoutes.js
@@ -0,0 +1,14 @@
+// imports
+const router = require('express').Router()
+const savedController = require('../controllers/savedController')
+
+// list all saved books for logged in user
+router.get('/saved', savedController.listSavedBooks)
+
+// toggle favorite on a saved book
+router.post('saved/:savedId/favortite', savedController.toggleFavorites)
+
+// delete a saved book
+router.post('saved/:savedId/delete', savedController.deleteSaved)
+
+module.exports = router
